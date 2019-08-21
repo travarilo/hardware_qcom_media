bralinx/hardware_qcom_media-caf-sdm660

media: use omnirom's pathmap

Android.mk
@@ -1,3 +1,4 @@
+ifeq ($(TARGET_QCOM_MEDIA_VARIANT),caf-sdm660)
 QCOM_MEDIA_ROOT := $(call my-dir)
 
 #Compile these for all targets under QCOM_BOARD_PLATFORMS list.
@@ -20,4 +21,5 @@ ifeq ($(TARGET_BOARD_AUTO),true)
 include $(QCOM_MEDIA_ROOT)/libsidebandstreamhandle/Android.mk
 endif
 
+endif
 endif

travarilo/hardware_qcom_media

Android.mk
@@ -1,4 +1,4 @@
-ifeq ($(call my-dir),$(call project-path-for,qcom-media))
+ifeq ($(TARGET_QCOM_MEDIA_VARIANT),caf-msm8998)
 
 QCOM_MEDIA_ROOT := $(call my-dir)
 